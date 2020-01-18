1b7317d5cdb316a065885f72748404fa9772c623
Nate Wessel <bike756@gmail.com>
2020-01-18 13:46:16 -0500

    remove old files - not used any more

--- old/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div id="r404">
-	<h1>Nope. Couldn't find that.</h1>
-	<p>Maybe try searching?</p>
-	<?php get_search_form(); ?>
-</div> <!--.r404-->
-
-<?php get_footer(); ?>
--- old/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="/">
-	<input type="text" class="field" name="s" placeholder="Search" />
-	<input type="submit" value='Search' />
-</form>
