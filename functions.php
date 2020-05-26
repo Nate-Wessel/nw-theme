a6b5c61c52974e81339888b0691da2de551ca826
Nate Wessel <bike756@gmail.com>
2020-05-26 14:25:14 -0400

    create metabox with select element

--- functions.php
+++ functions.php
@@ -20,4 +20,38 @@ register_nav_menu( 'main', "It's the only menu" );
 
 add_post_type_support( 'page', 'excerpt' );
 
+function adding_custom_meta_boxes( $post_type, $post ) {
+	add_meta_box(
+		"nw_related_posts",      # ID
+		"Related Posts",         # metabox title
+		"nw_related_posts_meta", # callback function to display box contents
+		"page",                  # post type effected
+		"side", "low",           # location, priority
+		null                     # callback args
+	);
+}
+add_action( 'add_meta_boxes', 'adding_custom_meta_boxes', 10, 2 );
+
+function nw_related_posts_meta(){
+	# posts/pages to select from metabox 
+	$posts = get_posts(array( 
+		'post_type' => ['page','cv_project'], 
+		'numberposts' => -1 ,# return all
+		'orderby' => 'title',
+		'order' => 'ASC'
+	));
+	$selected_posts = get_post_meta($object->ID, "link_to", false);
+	?>
+	<div>
+		<p>Select related posts to display below page content:</p>
+		<select name="link_to[]" size='10' multiple>
+		<?php foreach($posts as $post){
+			$selected = in_array(strval($post->ID),$selected_posts) ? 'selected' : '';
+			echo "\t\t\t<option value='$post->ID' $selected>$post->post_title</option>\n";
+		}?>
+		</select>
+	</div>
+<?php
+}
+
 ?>
